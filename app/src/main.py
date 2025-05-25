1751643c0171f73ed9c65dfa28348beedd5c74ad
m1guelch <miguelcolladohidalgo@gmail.com>
2025-05-25 17:46:19 +0200

    Updating app´s visit counter system

--- app/src/main.py
+++ app/src/main.py
@@ -5,12 +5,22 @@ from db import get_visits, increment_visits
 # Create the Flask app
 app = Flask(__name__)
 
-# Define the root route
+# Define the main route
 @app.route('/')
 def home():
-    # Increment the visit count and pass it to the HTML template
+    count = get_visits()
+    return render_template("index.html", count=count)
+
+# Define the visit counter route
+@app.route('/visit')
+def visit():
     count = increment_visits()
     return render_template("index.html", count=count)
+
+# Define the route for K8s probes
+@app.route('/health')
+def health():
+    return "OK", 200
 
 # Run the app if this file is executed directly
 if __name__ == '__main__':
